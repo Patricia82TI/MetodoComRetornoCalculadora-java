3dac73184dd658a5330a9d9076017e22b0bdc17b
Patricia82TI <patricia.melloti@gmail.com>
2024-12-26 07:12:09 -0300

    Metodo com retorno calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+
+    public static int soma(int x, int y){
+       int resultado = x + y;
+       return resultado;
+    }
+
+    public static int subtracao(int x, int y){
+        return x - y;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        System.out.println("Soma: " + Calculadora.soma(10, 20));
+        System.out.println("Subtracao: " + Calculadora.subtracao(10, 5));
+    }
+}
